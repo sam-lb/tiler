672f44c58b7b8144ae26913ef0db6c25b279e63f
sam-lb <samlbrunacini@gmail.com>
2025-05-25 16:22:53 -0400

    added a function to make a b&w mesh

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 from PIL import Image
-from tiler import compute_adaptive_triangulation, compute_adaptive_triangulation_gif
+from tiler import compute_adaptive_triangulation, compute_adaptive_triangulation_gif, adaptive_triangulation_mesh_gif
 
-# IMAGE_NAME = "gradation.jpg"
-# INPUT_FILE = "./test_images/{}".format(IMAGE_NAME)
-# OUTPUT_FILE = "./out_images/{}_processed.jpg".format(IMAGE_NAME.split(".")[0])
+IMAGE_NAME = "gradation.jpg"
+INPUT_FILE = "./test_images/{}".format(IMAGE_NAME)
+OUTPUT_FILE = "./out_images/{}_processed.jpg".format(IMAGE_NAME.split(".")[0])
 
-GIF_NAME = "blob.gif"
-INPUT_FILE = "./test_images/{}".format(GIF_NAME)
-OUTPUT_FILE = "./out_images/{}_processed.gif".format(GIF_NAME.split(".")[0])
+# GIF_NAME = "cat.gif"
+# INPUT_FILE = "./test_images/{}".format(GIF_NAME)
+# OUTPUT_FILE = "./out_images/{}_processed_mesh2.gif".format(GIF_NAME.split(".")[0])
 
 if __name__ == "__main__":
-    # img = Image.open(INPUT_FILE).convert("RGB")
-    # compute_adaptive_triangulation(img, save_target=OUTPUT_FILE, num_adaptive_points=100)
+    img = Image.open(INPUT_FILE).convert("RGB")
+    compute_adaptive_triangulation(img, save_target=OUTPUT_FILE, num_adaptive_points=100)
 
-    gif = Image.open(INPUT_FILE)
-    compute_adaptive_triangulation_gif(gif, save_target=OUTPUT_FILE, num_adaptive_points=200)
+    # gif = Image.open(INPUT_FILE)
+    # adaptive_triangulation_mesh_gif(gif, save_target=OUTPUT_FILE, num_adaptive_points=2000)
--- tiler.py
+++ tiler.py
@@ -12,23 +12,25 @@ def _compute_variance_map(img_np, ksize=9):
     var_map = np.sum(var, axis=2)
     return var_map
 
-def compute_adaptive_triangulation(img, num_adaptive_points=5000, save_target=None):
-    img_np = np.array(img)
+def _adaptive_triangulation(img_np, num_adaptive_points):
     h, w = img_np.shape[:2]
-
     var_map = _compute_variance_map(img_np, ksize=9)
     var_map = gaussian_filter(var_map, sigma=3)
     prob_map = var_map / np.sum(var_map)
-
     indices = np.random.choice(h * w, size=num_adaptive_points, p=prob_map.ravel())
     ys, xs = np.unravel_index(indices, (h, w))
     adaptive_points = np.column_stack((xs, ys)).astype(np.float32)
-
     corner_pts = np.array([[0, 0], [0, h - 1],
                         [w - 1, 0], [w - 1, h - 1]])
     points = np.vstack((adaptive_points, corner_pts))
-
     tri = Delaunay(points)
+
+    return points, tri
+
+def compute_adaptive_triangulation(img, num_adaptive_points=5000, save_target=None):
+    img_np = np.array(img)
+    h, w = img_np.shape[:2]
+    points, tri = _adaptive_triangulation(img_np, num_adaptive_points)
     output = np.zeros_like(img_np)
 
     for simplex in tri.simplices:
@@ -43,10 +45,39 @@ def compute_adaptive_triangulation(img, num_adaptive_points=5000, save_target=No
     if not (save_target is None): output.save(save_target)
     return output
 
+def adaptive_triangulation_mesh(img, num_adaptive_points=5000, save_target=None):
+    img_np = np.array(img)
+    points, tri = _adaptive_triangulation(img_np, num_adaptive_points)
+    output = np.zeros_like(img_np)
+
+    for simplex in tri.simplices:
+        pts = points[simplex].astype(np.int32)
+        cv2.polylines(output, [pts], True, (255, 255, 255), 1)
+
+    output = Image.fromarray(output)
+    if not (save_target is None): output.save(save_target)
+    return output
+
 def compute_adaptive_triangulation_gif(gif, num_adaptive_points=5000, save_target=None, fps=30):
     frames = []
     for frame in ImageSequence.Iterator(gif):
         processed = compute_adaptive_triangulation(frame.convert("RGB"), num_adaptive_points)
+        frames.append(processed)
+
+    millis = int(1000 / fps)
+    
+    if not(save_target is None):
+        frames[0].save(
+            save_target, append_images=frames[1:],
+            duration=millis, loop=0
+        )
+
+    return frames
+
+def adaptive_triangulation_mesh_gif(gif, num_adaptive_points=5000, save_target=None, fps=30):
+    frames = []
+    for frame in ImageSequence.Iterator(gif):
+        processed = adaptive_triangulation_mesh(frame.convert("RGB"), num_adaptive_points)
         frames.append(processed)
 
     millis = int(1000 / fps)
